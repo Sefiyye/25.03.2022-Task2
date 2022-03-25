68f10d0c40bd0dad3791eb9ec06222bf2d989841
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-25 13:06:06 +0400

    task

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName;
+            string groupNo;
+
+            do
+            {
+                Console.Write("Enter your FullName please: ");
+                fullName = Console.ReadLine();
+            }
+            while (!Student.CheckFullname(fullName));
+
+            do
+            {
+                Console.Write("Enter your Group please: ");
+                groupNo = Console.ReadLine();
+            }
+            while (!Student.CheckGroupNo(groupNo));
+
+            Console.Write("Enter your age: ");
+            byte age = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("~~~~~~~~~~~~~~~");
+
+            Student student = new Student(fullName, groupNo, age);
+            student.Info();
+
+        }
+    }
+}
